659f5297e7d128e49fce0922e764590e79de0e1e
Sebastian Krümling <sebastian.kruemling@usu.com>
2024-03-21 14:52:01 +0100

    some docs

--- src/main/kotlin/io/github/julasoftware/ms/Options.kt
+++ src/main/kotlin/io/github/julasoftware/ms/Options.kt
@@ -1,9 +1,43 @@
 package io.github.julasoftware.ms
 
+/**
+ * Various options to control the conversion and output
+ */
 data class Options(
+    /**
+     * controls whether the text output is compact <code>20s</code> or verbose <code>20 seconds</code>
+     * 
+     * Default: false
+     */
     val longFormat: Boolean = false,
+
+    /**
+     * Option for rounding numbers or leave as is
+     *
+     * Default: true
+     */
     val roundNumbers: Boolean = true,
+
+    /**
+     * When numbers are rounded, this decimal precision is applied
+     *
+     * Default: 0
+     */
     val roundPrecision: Int = 0,
+
+    /**
+     * Option for splitting a larger time into its pieces.
+     * e.g. instead an output of <code>10 days</code> it will return <code>1 week 3 days</code>
+     *
+     * Default: false
+     */
     var splitTextOutput: Boolean = false,
+
+    /**
+     * When splitting is active it controls the delimiter of the segments in the string
+     * e.g. instead of <code>1 week 3 days</code> it will generate <code>1 week, 3 days</code> when set to ", "
+     *
+     * Default: " " (whitespace)
+     */
     var splitTextOutputChar: String = " "
 )
